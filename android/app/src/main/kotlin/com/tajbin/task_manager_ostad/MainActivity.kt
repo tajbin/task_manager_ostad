55242033eada627589ffd38a18d62ca7595f491c
tajbin <tajbin2013@gmail.com>
2024-06-04 20:00:39 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/tajbin/task_manager_ostad/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.tajbin.task_manager_ostad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/ui/utility/asset_paths.dart
@@ -0,0 +1,4 @@
+class AssetPaths{
+  static const String _basePath = 'assets';
+  static const String _imagePath = '$_basePath/images';
+}
